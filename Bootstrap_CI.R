8fdd637868e825dfbc81a40f77086c3fc9f8a9b6
Ruven94 <149207574+Ruven94@users.noreply.github.com>
2025-05-26 12:59:47 +0200

    Update Bootstrap_CI.R

--- Bootstrap_CI.R
+++ Bootstrap_CI.R
@@ -37,10 +37,10 @@ VIRF_result <- AVIRF(data = data,
                       timeforVIRF = timeforVIRF,
                       n.ahead = n.ahead,
                       asym = FALSE,
-                      bootsamp = 50000)
+                      simsamp = 50000)
 
 # save(VIRF_result, file = "VIRF_result.RData")
-load("VIRF_result.RData")
+load("Run 2/VIRF_result.RData")
 
 ### Bootstrap CI ###
 result_list <- mclapply(1:R, function(k) {
@@ -52,7 +52,8 @@ result_list <- mclapply(1:R, function(k) {
         timeforVIRF = timeforVIRF,
         n.ahead = n.ahead,
         asym = FALSE,
-        bootsamp = 50000,
+        simsamp = 50000,
+        cores = 1,
         C_boot = bootstrap_bekk_result$C0[,,k],
         A_boot = bootstrap_bekk_result$A[,,k],
         G_boot = bootstrap_bekk_result$G[,,k])
@@ -66,20 +67,22 @@ for (k in 1:R) {
 }
 
 # save(all_virfs, all_cirfs, file = "VIRF_sim_result.RData")
-load("VIRF_sim_result.RData")
-
+load("Run 3/VIRF_sim_result_bootstrap.RData")
+# load("Run 2/VIRF_sim_result_withoutBEKK.RData")
 
 ##################
 ### Evaluation ###
 ##################
 virf_CI_lower <- matrix(0, dim(all_virfs)[1], dim(all_virfs)[2])
 virf_CI_upper <- matrix(0, dim(all_virfs)[1], dim(all_virfs)[2])
+virf_mean <- matrix(0, dim(all_virfs)[1], dim(all_virfs)[2])
 alpha <- 0.1
 
 for (j in 1:dim(all_virfs)[2]) {
   for (i in 1:dim(all_virfs)[1]) {
     virf_CI_lower[i,j] <- quantile(all_virfs[i,j,],alpha)
     virf_CI_upper[i,j] <- quantile(all_virfs[i,j,],1-alpha)
+    virf_mean[i,j] <- mean(all_virfs[i,j,])
   }
 }
 
@@ -89,15 +92,16 @@ for (j in 1:dim(all_virfs)[2]) {
 df_list <- list()
 for (i in 1:3) {
   df_temp <- data.frame(
-    index = 1:nrow(VIRF_result$VIRF),
-    response = paste0("Response_", i),
-    est = VIRF_result$VIRF[, i],
-    lower = virf_CI_lower[, i],
-    upper = virf_CI_upper[, i]
+    index           = 1:nrow(VIRF_result$VIRF),
+    response        = paste0("Response_", i),
+    est             = virf_mean[, i],
+    lower           = virf_CI_lower[, i],
+    upper           = virf_CI_upper[, i],
+    theoretical_virf = VIRF_result$VIRF[, i]   # e.g. your “true” VIRF
   )
   df_list[[i]] <- df_temp
 }
-df_all <- bind_rows(df_list)
+df_all <- dplyr::bind_rows(df_list)
 
 ggplot(df_all, aes(x = index, y = est)) +
   geom_line(size = 1) +  
@@ -107,4 +111,3 @@ ggplot(df_all, aes(x = index, y = est)) +
   labs(x = "Index", y = "Wert", 
        title = "VIRF mit Konfidenzbändern") +
   theme(plot.title = element_text(hjust = 0.5))
-
